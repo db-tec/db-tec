4b2c0ac615d91af8ad9ec3115dbc70621ff49ba9
db-tec <eng.davidborges@gmail.com>
2020-03-29 16:56:35 -0300

    Criando apps documento e hora extra

--- gestao_rh/settings.py
+++ gestao_rh/settings.py
@@ -29,7 +29,9 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'apps.empresas',
     'apps.funcionarios',
-    'apps.departamentos'
+    'apps.departamentos',
+    'apps.documentos',
+    'apps.registro_hora_extra'
 
 ]
 
